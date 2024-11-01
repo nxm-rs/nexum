9c9f031e0d3ae801a1550af6446943c06b244290
mfw78 <mfw78@protonmail.com>
2024-11-01 15:35:05 +0000

    fix: port wrapper functions

--- crates/chrome-sys/src/port.rs
+++ crates/chrome-sys/src/port.rs
@@ -3,25 +3,32 @@ use wasm_bindgen::prelude::*;
 
 // Rust wrappers
 
-// Updated wrapper function that accepts a pre-made `&Function` reference
-pub fn port_add_on_disconnect_listener(port: JsValue, callback: &Function) -> Result<(), JsValue> {
-    // Retrieve the `addListener` function from the `port` object
-    let add_listener_func = Reflect::get(&port, &JsValue::from_str("addListener"))?
-        .dyn_into::<Function>()?;
+// Updated wrapper function that accesses `port.onDisconnect.addListener`
+pub fn add_on_disconnect_listener(port: JsValue, callback: &Function) -> Result<(), JsValue> {
+    // Retrieve the `onDisconnect` object from `port`
+    let on_disconnect = Reflect::get(&port, &JsValue::from_str("onDisconnect"))?;
 
-    // Call `addListener` on `port` with `callback` as the argument
-    add_listener_func.call1(&port, callback)?;
+    // Retrieve the `addListener` function from `onDisconnect`
+    let add_listener_func =
+        Reflect::get(&on_disconnect, &JsValue::from_str("addListener"))?.dyn_into::<Function>()?;
+
+    // Call `addListener` on `onDisconnect` with `callback` as the argument
+    add_listener_func.call1(&on_disconnect, callback)?;
 
     Ok(())
 }
 
+// Wrapper function to access `port.onDisconnect.removeListener`
 pub fn remove_on_disconnect_listener(port: JsValue, callback: &Function) -> Result<(), JsValue> {
-    // Retrieve the `removeListener` function from the `port` object
-    let remove_listener_func = Reflect::get(&port, &JsValue::from_str("removeListener"))?
+    // Retrieve the `onDisconnect` object from `port`
+    let on_disconnect = Reflect::get(&port, &JsValue::from_str("onDisconnect"))?;
+
+    // Retrieve the `removeListener` function from `onDisconnect`
+    let remove_listener_func = Reflect::get(&on_disconnect, &JsValue::from_str("removeListener"))?
         .dyn_into::<Function>()?;
 
-    // Call `removeListener` on `port` with `callback` as the argument
-    remove_listener_func.call1(&port, callback)?;
+    // Call `removeListener` on `onDisconnect` with `callback` as the argument
+    remove_listener_func.call1(&on_disconnect, callback)?;
 
     Ok(())
 }
@@ -29,9 +36,9 @@ pub fn remove_on_disconnect_listener(port: JsValue, callback: &Function) -> Resu
 // TODO: Documentation on why this uses reflect versus the others that don't
 pub fn post_message(port: &JsValue, message: JsValue) -> Result<(), JsValue> {
     // Retrieve `postMessage` as a Function from the port object
-    let post_message_func = Reflect::get(port, &JsValue::from_str("postMessage"))?
-        .dyn_into::<Function>()?;
-    
+    let post_message_func =
+        Reflect::get(port, &JsValue::from_str("postMessage"))?.dyn_into::<Function>()?;
+
     // Call `postMessage` on `port` with `message` as the argument
     post_message_func.call1(port, &message)?;
 
