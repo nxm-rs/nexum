fb40772c4fa82c08f967609ffc3b7547d561f99d
Meet Mangukiya <meet@flamy.dev>
2025-05-18 13:47:44 +0530

    feat: async tui

--- crates/tui/src/main.rs
+++ crates/tui/src/main.rs
@@ -1,6 +1,15 @@
-use std::{fs::OpenOptions, path::PathBuf};
+use std::{fs::OpenOptions, path::PathBuf, time::Duration};
 
+use crossterm::event::{Event, EventStream, KeyCode};
 use eyre::OptionExt;
+use futures::StreamExt;
+use ratatui::{
+    layout::{Constraint, Layout},
+    style::Stylize,
+    text::Line,
+    widgets::{Block, Borders},
+    DefaultTerminal, Frame,
+};
 use tracing_subscriber::EnvFilter;
 
 /// Returns the base config directory for nexum. It also creates the directory
@@ -27,7 +36,7 @@ fn tui_logger() -> impl std::io::Write {
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> eyre::Result<()> {
     tracing_subscriber::fmt()
         .with_writer(tui_logger)
         .with_env_filter(EnvFilter::from_default_env())
@@ -37,4 +46,50 @@ async fn main() {
     tracing::error!("error");
     tracing::debug!("debug");
     tracing::trace!("trace");
+
+    let terminal = ratatui::init();
+    let app_result = App::default().run(terminal).await;
+    ratatui::restore();
+    app_result
+}
+
+#[derive(Default)]
+struct App {
+    pub should_quit: bool,
+}
+
+impl App {
+    const FRAMES_PER_SECOND: u64 = 10;
+
+    pub async fn run(mut self, mut terminal: DefaultTerminal) -> eyre::Result<()> {
+        let period = Duration::from_secs_f32(1.0 / (Self::FRAMES_PER_SECOND as f32));
+        let mut interval = tokio::time::interval(period);
+        let mut events = EventStream::new();
+
+        while !self.should_quit {
+            tokio::select! {
+                _ = interval.tick() => { terminal.draw(|f| self.render(f))?; },
+                Some(Ok(event)) = events.next() => self.handle_event(&event),
+            }
+        }
+        Ok(())
+    }
+
+    fn render(&self, frame: &mut Frame) {
+        let vertical = Layout::vertical([Constraint::Length(1), Constraint::Fill(1)]);
+        let [title_area, body_area] = vertical.areas(frame.area());
+        let title = Line::from("Ratatui async example").centered().bold();
+        let block = Block::new().borders(Borders::ALL);
+        frame.render_widget(title, title_area);
+        frame.render_widget(block, body_area);
+    }
+
+    fn handle_event(&mut self, event: &Event) {
+        if let Some(key) = event.as_key_press_event() {
+            match key.code {
+                KeyCode::Char('q') | KeyCode::Esc => self.should_quit = true,
+                _ => {}
+            }
+        }
+    }
 }
